4b4dd133a60d24702790a91d42e71be28f5dfb9f
NicolasTOISON <toisonnicolas@gmail.com>
2014-02-10 19:07:23 +0100

    modification nicolas toison

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,13 +1,34 @@
 package main;
+import java.awt.BorderLayout;
 
-public class Main {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Main extends JPanel {
 
-	}
+	public Main() {
 
+    super(true);
+    this.setLayout(new BorderLayout());
+    JSlider slider = new JSlider(JSlider.HORIZONTAL, 0, 50, 25);
+
+    slider.setMinorTickSpacing(2);
+    slider.setMajorTickSpacing(10);
+    slider.setPaintTicks(true);
+    slider.setPaintLabels(true);
+
+    // We'll just use the standard numeric labels for now...
+    slider.setLabelTable(slider.createStandardLabels(10));
+
+    add(slider, BorderLayout.CENTER);
+  }
+
+  public static void main(String s[]) {
+    JFrame frame = new JFrame("Slider Example");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setContentPane(new Main());
+    frame.pack();
+    frame.setVisible(true);
+  }
 }
