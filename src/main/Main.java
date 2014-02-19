9d67a275e10a9a61abc34f3830b0aa82e577354f
NicolasTOISON <toisonnicolas@gmail.com>
2014-02-19 12:10:04 +0100

    Reset JFrame

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,34 +1,15 @@
-package main;
-import java.awt.BorderLayout;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jframe;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JSlider;
-
-public class Main extends JPanel {
-
-	public Main() {
-
-    super(true);
-    this.setLayout(new BorderLayout());
-    JSlider slider = new JSlider(JSlider.HORIZONTAL, 0, 50, 25);
-
-    slider.setMinorTickSpacing(2);
-    slider.setMajorTickSpacing(10);
-    slider.setPaintTicks(true);
-    slider.setPaintLabels(true);
-
-    // We'll just use the standard numeric labels for now...
-    slider.setLabelTable(slider.createStandardLabels(10));
-
-    add(slider, BorderLayout.CENTER);
-  }
-
-  public static void main(String s[]) {
-    JFrame frame = new JFrame("Slider Example");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setContentPane(new Main());
-    frame.pack();
-    frame.setVisible(true);
-  }
+/**
+ *
+ * @author Franck
+ */
+public class Main {
+    public static void main(String[] args) {
+        Fenetre f=new Fenetre();
+    }
 }
